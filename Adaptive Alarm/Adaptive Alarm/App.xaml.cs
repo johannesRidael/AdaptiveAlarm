033e5ff8a5ef3a678aea30213cbb9e51fafbe2f7
Ariel <arieloconitrillo@gmail.com>
2022-03-01 17:13:43 -0700

    Test

---
+++ Adaptive Alarm/Adaptive Alarm/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace Adaptive_Alarm
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Adaptive Alarm/Adaptive Alarm/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Adaptive_Alarm
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
