cb28908d209bef0849c7b0141b6d25dec876f8ad
bdl3700-desktop <bdl3700@comcast.net>
2022-03-23 11:02:17 -0600

    Finished adding code to download Fitbit data for testing purposes.

--- DataCollection_Testing/Program.cs
+++ DataCollection_Testing/Program.cs
@@ -6,6 +6,10 @@ using System.Text;
 using System.Threading;
 using Fitbit.Api.Portable;
 using Fitbit.Models;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 
 namespace DataCollection_Testing
 {
@@ -47,15 +51,77 @@ namespace DataCollection_Testing
 
             Fitbit.Api.Portable.OAuth2.OAuth2AccessToken t = ah.ExchangeAuthCodeForAccessTokenAsync(code).Result;
 
-            
+            // build a fitbit client and make calls for the required data
             FitbitClient client = new FitbitClient(cred, t);
 
-            Fitbit.Api.Portable.Models.SleepLogDateBase sleep = client.GetSleepDateAsync(new DateTime(2022, 03, 15)).Result;
-            int occurance = 0;
-            Console.WriteLine($"Sleep Data Summary: Sleep event {occurance} is main sleep: {sleep.Sleep[occurance].IsMainSleep}, duration {sleep.Sleep[occurance].Duration} milliseconds, efficiency score {sleep.Sleep[occurance].Efficiency}"); // this sleep.Sleep object is a list of sleep events (times the user slept) find the main sleep event and use its attributes to read what you want. Includes all sleep stages and wake events in that sleep period.
+            //Fitbit.Api.Portable.Models.SleepLogDateBase sleep = client.GetSleepDateAsync(new DateTime(2022, 03, 15)).Result;
+            //int occurance = 0;
+            //Console.WriteLine($"Sleep Data Summary: Sleep event {occurance} is main sleep: {sleep.Sleep[occurance].IsMainSleep}, duration {sleep.Sleep[occurance].Duration} milliseconds, efficiency score {sleep.Sleep[occurance].Efficiency}"); // this sleep.Sleep object is a list of sleep events (times the user slept) find the main sleep event and use its attributes to read what you want. Includes all sleep stages and wake events in that sleep period.
+            //string sleepJson = JsonSerializer.Serialize(sleep);
+            //Console.WriteLine(sleepJson);
 
-            Fitbit.Models.HeartActivitiesIntraday heart = client.GetHeartRateIntraday(new DateTime(2022, 03, 15), HeartRateResolution.fifteenMinute).Result;
-            Console.WriteLine($"Heart Rate Data Summary: {heart.DatasetType}, resting heart rate {heart.ActivitiesHeart.Value}, heart rate {heart.Dataset[0].Value} at time {heart.Dataset[0].Time}");
+            //Fitbit.Models.HeartActivitiesIntraday heart = client.GetHeartRateIntraday(new DateTime(2022, 03, 15), HeartRateResolution.fifteenMinute).Result;
+            //Console.WriteLine($"Heart Rate Data Summary: {heart.DatasetType}, resting heart rate {heart.ActivitiesHeart.Value}, heart rate {heart.Dataset[0].Value} at time {heart.Dataset[0].Time}");
+            //string heartJson = JsonSerializer.Serialize(heart);
+            //Console.WriteLine(heartJson);
+
+
+            /*
+             * TODO: Add code to:
+             * 1) Read from a local /data/ directory and determine what days need to be downloaded
+             * 2) Make calls to the Fitbit API to download those days
+             * 3) Serialize the data back to JSON and output it to the /data/ directory mentioned above
+             */
+
+            string[] filePaths = Directory.GetFiles(@"..\..\..\data\", "*.json", SearchOption.TopDirectoryOnly);
+            HashSet<DateTime> daysPresent = new HashSet<DateTime>();
+            foreach (string p in filePaths)
+            {
+                string[] pTokens = p.Split(separator: '\\');
+                string fileName = pTokens[pTokens.Length - 1].Split('.')[0];
+                //Console.WriteLine($"{p}: {fileName}");
+                if (DateTime.TryParse(fileName, out DateTime date))
+                {
+                    daysPresent.Add(date);
+                    Console.WriteLine($"Found date json file: {fileName}");
+                }
+                else // not a date
+                {
+                    Console.WriteLine($"Found non-date json file in data folder: {fileName}");
+                }
+            }
+
+            int days = (DateTime.Today - DateTime.Parse("2022-02-20")).Days;
+            HashSet<DateTime> daysAbsent = new HashSet<DateTime>();
+            for (int i = 0; i < days; i++)
+            {
+                DateTime day = DateTime.Parse("2022-02-20").AddDays(i);
+                if (!daysPresent.Contains(day))
+                {
+                    daysAbsent.Add(day);
+                    Console.WriteLine($"absent day found: {day.ToString("yyyy-MM-dd")}");
+                }
+            }
+
+            foreach (DateTime day in daysAbsent)
+            {
+
+                Console.WriteLine($"retreiving and saving data for {day.ToString("yyyy-MM-dd")}");
+                // sleep data
+                Fitbit.Api.Portable.Models.SleepLogDateBase daysSleep = client.GetSleepDateAsync(day).Result;
+                string daysSleepJson = JsonSerializer.Serialize(daysSleep);
+                //Console.WriteLine(daysSleepJson);
+                File.WriteAllTextAsync($"..\\..\\..\\data\\{day.ToString("yyyy-MM-dd")}.sleep.json", daysSleepJson);
+
+                //// heartrate data
+                Fitbit.Models.HeartActivitiesIntraday daysHeart = client.GetHeartRateIntraday(day, HeartRateResolution.fifteenMinute).Result;
+                string daysHeartJson = JsonSerializer.Serialize(daysHeart);
+                //Console.WriteLine(daysHeartJson);
+                File.WriteAllTextAsync($"..\\..\\..\\data\\{day.ToString("yyyy-MM-dd")}.heart.json", daysHeartJson);
+            }
+
+            Console.WriteLine("\r\n\r\nData collection completed. Press Enter to Exit.");
+
             Console.ReadLine();
         }
     }
