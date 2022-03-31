c7afc3bf96a6478efe499e111ddd773fa4f4d7d6
eeeeee <elijah@andersonplanet.net>
2022-03-30 22:54:15 -0600

    G&C

--- Adaptive Alarm/Adaptive Alarm/App.xaml.cs
+++ Adaptive Alarm/Adaptive Alarm/App.xaml.cs
@@ -23,5 +23,76 @@ namespace Adaptive_Alarm
         protected override void OnResume()
         {
         }
+
+        private void nearestMidnight(DateTime dt)
+        {
+            DateTime p1 = new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0);
+            DateTime p2 = new DateTime(dt.Year, dt.Month, dt.Day + 1, 0, 0, 0);
+            TimeSpan dif1 = Math.Abs(p1 - dt);
+            TimeSpan dif2 = Math.Abs(p2 - dt);
+            if (dif1 < dif2)
+            {
+                return p1;
+            }
+            else
+            {
+                return p2;
+            }
+            //TODO: Add handling for nonconventional sleep times, i.e. graveyard workers
+
+        }
+
+        int[] attempts = new int[60];
+        int[] scores = new int[60];
+        int ind = 0;
+        int optCycleTime = 0;
+        int awakeTime = 10; // in minutes
+        int step = 15;
+        protected async void findAlarmTime()
+        {
+            int CurrCycle = -1;
+            if(optCycleTime == 0)
+            {
+                // use step and prev to find next attempt
+                if (ind == 0)
+                {
+                    attempts[ind] = 90;
+                    CurrCycle = 90;
+                }
+                else if (ind == 1)
+                {
+                    attempts[ind] = 105;
+                    CurrCycle = 105;
+                }
+                else
+                {
+                    if (scores[ind-1] <= scores[ind-2])
+                    {
+                        
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+                CurrCycle = optCycleTime;
+            }
+            DateTime now = DateTime.Now;
+            TimeSpan wakeBy = getWakeBy();
+            DateTime nm = nearestMidnight(now);
+            DateTime wakeBy = nm + wakeBy;
+            TimeSpan ttSleep = wakeBy - now;
+            int minToSleep = ttSleep.TotalMinutes;
+
+            int timeTillAlarm = awakeTime;
+            while(timeTillAlarm < minToSleep - CurrCycle)
+            {
+                timeTillAlarm = timeTillAlarm + CurrCycle;
+            }
+            //timeTillAlarm is now how many minutes before we should wake the user
+        }
     }
 }
